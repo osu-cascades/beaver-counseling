ce9078b983673e177a05d6e0058aa553134635ec
JackEWilson <jack.wilson.east@gmail.com>
2017-10-25 09:11:02 -0700

    set root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+	root 'high_voltage/pages#show', id: 'welcome'
+
   get 'pages/welcome' => 'high_voltage/pages#show', id: 'welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
