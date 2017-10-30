b58bc5144c69e385cfdbc750970ed7c7c8a9e62d
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-30 09:22:43 -0700

    aded login link to admin home and counselor home

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	root 'high_voltage/pages#show', id: 'welcome'
 
   get 'pages/welcome' => 'high_voltage/pages#show', id: 'welcome'
+	get 'pages/adminhome' => 'high_voltage/pages#show', id: 'adminhome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
