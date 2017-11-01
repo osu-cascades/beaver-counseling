1a3dae52af4b40a4e7ef9281eb71b1ab3ccfab19
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-31 18:06:28 -0700

    added links to admin home

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 	get 'pages/adminhome' => 'high_voltage/pages#show', id: 'adminhome'
 	get 'pages/newclientregistration' => 'high_voltage/pages#show', id: 'newclientregistration'
 	get 'pages/login' => 'high_voltage/pages#show', id: 'login'
+	get 'pages/adminviewclients' => 'high_voltage/pages#show', id: 'adminviewclients'
+	get 'pages/adminclientreports' => 'high_voltage/pages#show', id: 'adminclientreports'
+	get 'pages/adminmanageusers' => 'high_voltage/pages#show', id: 'adminmanageusers'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
