db810668a52a280b381249aabe758699fd97c537
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-10-31 18:01:03 -0700

    added links

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   get 'pages/welcome' => 'high_voltage/pages#show', id: 'welcome'
 	get 'pages/adminhome' => 'high_voltage/pages#show', id: 'adminhome'
+	get 'pages/newclientregistration' => 'high_voltage/pages#show', id: 'newclientregistration'
+	get 'pages/login' => 'high_voltage/pages#show', id: 'login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
