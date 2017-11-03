97545c27908d42df67ed2beb99ea18e7a4680853
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-03 10:06:50 -0700

    removed admin view clients page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
+
 	root 'high_voltage/pages#show', id: 'welcome'
 
+  get 'pages/searchresults' => 'high_voltage/pages#show', id: 'searchresults'
   get 'pages/welcome' => 'high_voltage/pages#show', id: 'welcome'
 	get 'pages/adminhome' => 'high_voltage/pages#show', id: 'adminhome'
 	get 'pages/newclientregistration' => 'high_voltage/pages#show', id: 'newclientregistration'
 	get 'pages/login' => 'high_voltage/pages#show', id: 'login'
-	get 'pages/adminviewclients' => 'high_voltage/pages#show', id: 'adminviewclients'
 	get 'pages/adminclientreports' => 'high_voltage/pages#show', id: 'adminclientreports'
 	get 'pages/adminmanageusers' => 'high_voltage/pages#show', id: 'adminmanageusers'
 	get 'pages/adminviewmore' => 'high_voltage/pages#show', id: 'adminviewmore'
