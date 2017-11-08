57e04416552f061238059069b1a19638e91d6b98
Yong Bakos <ybakos@humanoriented.com>
2017-11-08 08:43:08 -0800

    Change keys of Clientnt fixture objects.

--- test/controllers/clients_controller_test.rb
+++ test/controllers/clients_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ClientsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @client = clients(:one)
+    @client = clients(:valid)
   end
 
   test "should get index" do
