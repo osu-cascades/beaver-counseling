0641dbe0575a449b073c22eb8900ca9e7cca6fb8
Yong Bakos <ybakos@humanoriented.com>
2017-11-08 08:46:52 -0800

    Tests: Add initial Client failing model tests.

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Client should exist" do
+    assert_nothing_raised { Client.new }
+  end
+
+  test "Client should have a first_name" do
+    client_name = "Jack"
+    client = clients(:valid)
+    assert_equal(client_name, client.first_name)
+  end
+
+  test "Client without a first name is not valid" do
+    invalid_client = clients(:invalid)
+    assert_not(invalid_client.valid?)
+  end
+
+  test "Client first name must be a string" do
+    invalid_client = clients(:invalid)
+    assert_not(invalid_client.valid?)
+  end
+
 end
