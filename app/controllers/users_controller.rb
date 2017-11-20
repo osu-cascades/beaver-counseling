36f3e2329fe84772e80ab4ce87b631468e89beea
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-19 17:21:09 -0800

    fixed users controller, admin can now create counselors fixes #36

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,21 @@ class UsersController < ApplicationController
 
   def create
     # user create code (can't get here if not admin)
-    puts request.raw_post
+    #puts request.raw_post
     @user = User.new
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.role = params[:user][:role]
 
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to pages_adminmanageusers_path, notice: 'User was successfully created.' }
+        format.json { render inline: "location.reload();", status: :created }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
