ac2a75b5c2060664e763c6c320a556019421e62e
JackEWilson <jack.wilson.east@gmail.com>
2017-11-22 17:19:01 -0800

    permitted user_id for client, and displayed clients on counselor home based off of if the client belonged to them

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -82,6 +82,6 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:first_name)
+      params.require(:client).permit(:first_name, :user_id)
     end
 end
