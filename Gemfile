05301c0880f3436174ea79471749da239030f469
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-28 19:38:00 -0800

    added chart.js

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem 'jquery-rails'
+gem "chartkick"
 # bootstrap all the way!!!
 gem 'bootstrap-sass', '3.3.7'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require Chart.bundle
+//= require chartkick
 //= require bootstrap-sprockets
 //= require rails-ujs
-//= require turbolinks
