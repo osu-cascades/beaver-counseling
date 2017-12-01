0e5cc2ee1f118853240e0af7cf3b29662a1decb9
Broc Nickodemus <nickodeb@oregonstate.edu>
2017-11-30 20:09:23 -0800

    added admin generation in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(
+  email: 'admin@gmail.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: 1
+)
+user.save!
