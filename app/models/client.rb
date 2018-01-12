cc9026d604b8789e1e69feae8e9589286bb935a9
Yong Bakos <ybakos@humanoriented.com>
2018-01-12 09:54:42 -0800

    Client: Add callback and stub. cc: @bnickodemus @wilsonja1

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,12 @@
 class Client < ApplicationRecord
-  belongs_to :user
+  belongs_to :user # belongs_to :counselor, :class_name: 'User'
   validates :first_name, format: { with: /\A[a-zA-Z]+\z/}
+  after_initialize :assign_default_counselor
+
+  private
+
+    def assign_default_counselor
+      # counselor = User.default_counselor
+    end
 
 end
