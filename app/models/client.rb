66dd5bae3cd125c572a6fc03aa0085837b577c4c
Yong Bakos <ybakos@humanoriented.com>
2018-01-12 10:01:34 -0800

    Client: Add to_s.

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,10 @@ class Client < ApplicationRecord
   validates :first_name, format: { with: /\A[a-zA-Z]+\z/}
   after_initialize :assign_default_counselor
 
+  def to_s
+    "#{first_name} #{last_name}"
+  end
+
   private
 
     def assign_default_counselor
