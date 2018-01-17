b7553024ac7577977adfe0b8e8cf46d670d19248
Broc Nickodemus <nickodeb@oregonstate.edu>
2018-01-17 09:46:09 -0800

    removed after init

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,6 @@
 class Client < ApplicationRecord
   belongs_to :user, optional: true # belongs_to :counselor, :class_name: 'User'
   validates :first_name, format: { with: /\A[a-zA-Z]+\z/}
-  after_initialize :assign_default_counselor
 
   def to_s
     "#{first_name} #{last_name}"
