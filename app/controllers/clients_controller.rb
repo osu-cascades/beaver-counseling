793b1408be8a96e9a64f9c6ef74ab466349f2285
Broc Nickodemus <nickodeb@oregonstate.edu>
2018-01-21 23:29:41 -0800

    prevent counselors from editing and viewing clients that aren't theirs fixes #57

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,6 +5,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
+    authorize_admin
     @clients = Client.all
     @users = User.all
   end
@@ -12,6 +13,7 @@ class ClientsController < ApplicationController
   # GET /clients/1
   # GET /clients/1.json
   def show
+    authorize_admin
   end
 
   # GET /clients/new
@@ -22,8 +24,10 @@ class ClientsController < ApplicationController
 
   # GET /clients/1/edit
   def edit
-    @clients = Client.all
-    @Users = User.all
+    @client = Client.find(params[:id])
+    if current_user.id != @client.counselor_id
+      redirect_to root_path, alert: 'Admins only!'
+    end
   end
 
   # POST /clients
@@ -84,8 +88,13 @@ class ClientsController < ApplicationController
   # PATCH/PUT /clients/1
   # PATCH/PUT /clients/1.json
   def update
-    # only the admin can change the user the client is assigned to
+    # precautionary: post would need to be sent from postman?
+    @client = Client.find(params[:id])
+    if current_user.id != @client.counselor_id
+      redirect_to root_path, alert: 'Admins only!'
+    end
 
+    # only the admin can change the user the client is assigned to
     respond_to do |format|
       if (@client.counselor_id != params[:client][:counselor_id] && !@client.counselor_id.nil?)
         @client.previous_counselor.push(User.find(@client.counselor_id).name)#if so add counselor name
@@ -114,6 +123,12 @@ class ClientsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_client
       @client = Client.find(params[:id])
+    end
+
+    def authorize_admin
+      if (current_user.admin? == false)
+        redirect_to root_path, alert: 'Admins only!'
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
