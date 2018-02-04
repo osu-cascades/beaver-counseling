a23a7db2da91acb867bc60b500c2c27984c03a15
JackEWilson <jack.wilson.east@gmail.com>
2018-02-04 10:18:11 -0800

    trying out system tests, not passing

--- test/system/clients_test.rb
+++ test/system/clients_test.rb
@@ -1,9 +1,16 @@
 require "application_system_test_case"
 
 class ClientsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit clients_url
-  #
-  #   assert_selector "h1", text: "Client"
-  # end
+  test "visiting the index" do
+    visit clients_url
+  
+    assert_selector "h1", text: "Welcome to OSU Cascades Counseling Clinic"
+  end
+
+  test "visiting the homepage" do
+  	visit welcome
+
+  	assert_selector "h1", text: "Welcome to OSU Cascades Counseling Clinic"
+  end
+
 end
