0db519964ba55caf4ebbc1e7e679c07dd62744bb
JackEWilson <jack.wilson.east@gmail.com>
2018-02-19 10:16:40 -0800

    deleted rest of ckeditor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
 	root 'high_voltage/pages#show', id: 'welcome'
 
 	devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
