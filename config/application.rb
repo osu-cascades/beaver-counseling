a05fb812604f7bd530e4ee1fcce9691b50c878b3
Broc Nickodemus <nickodeb@oregonstate.edu>
2018-02-19 19:30:38 -0800

    added ckeditor models back

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module BeaverCounseling
 
     #config.assets.precompile += Ckeditor.assets
     #config.assets.precompile += %w( ckeditor/* )
-    #config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
