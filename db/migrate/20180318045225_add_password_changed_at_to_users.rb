f238f76aa1435004915c9c682fd0e141630edbce
JackEWilson <jack.wilson.east@gmail.com>
2018-03-17 22:24:51 -0700

    fixed migration

--- db/migrate/20180318045225_add_password_changed_at_to_users.rb
+++ db/migrate/20180318045225_add_password_changed_at_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordChangedAtToUsers < ActiveRecord::Migration[5.1]
   def change
-  	t.datetime :password_changed_at
+  	add_column :users, :password_changed_at, :datetime
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180304225454) do
+ActiveRecord::Schema.define(version: 20180318045225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20180304225454) do
     t.integer "role"
     t.string "name"
     t.boolean "is_archived"
+    t.datetime "password_changed_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
