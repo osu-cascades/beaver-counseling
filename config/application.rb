bad9d7f8119243b55c15d9dd66a07d4e56b45e34
JackEWilson <jack.wilson.east@gmail.com>
2018-04-14 22:44:13 -0700

    added comments about removing note: google:cloud:logging is disabled, still not removed, references #93

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,21 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+#getting error undefined method `google_cloud'
+# Rails.application.configure do |config|
+#   config.google_cloud.use_debugger = false
+#   config.google_cloud.use_logging = false
+#   config.google_cloud.use_trace = false
+# end
+
+#look at https://googlecloudplatform.github.io/google-cloud-ruby/#/docs/stackdriver/v0.14.0/guides/instrumentation_configuration
+#https://cloud.google.com/logging/docs/setup/ruby
+
 module BeaverCounseling
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
 
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
@@ -19,3 +30,5 @@ module BeaverCounseling
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+
