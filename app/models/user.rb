df1386ac0d529143aafee3003c800c2730a582b2
Yong Bakos <ybakos@humanoriented.com>
2018-05-02 07:39:24 -0400

    User: Add whitespace.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,17 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :clients
+
   devise :password_expirable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   enum role: %w(admin counselor)
+
+  has_many :clients
+
   def self.find_version_counselor(version)
     begin
       find(version.terminator)
     rescue
-      @error_message="client has no counselor"
+      @error_message = "client has no counselor"
     end
   end
 
@@ -17,4 +19,5 @@ class User < ApplicationRecord
   def active_for_authentication?
     super and !self.is_archived? #determine "active" state using our own "is_archived" column
   end
+
 end
