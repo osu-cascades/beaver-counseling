8fb66f85ac683b2b7e88b8bb848fbf865773c7c5
Broc Nickodemus <nickodeb@oregonstate.edu>
2018-05-15 20:32:39 -0700

    added family member validations fixes #114

--- app/models/family_member.rb
+++ app/models/family_member.rb
@@ -1,3 +1,6 @@
 class FamilyMember < ApplicationRecord
   belongs_to :client
+
+  validates :name, format: { with: /\A[a-zA-Z\'.-]+\z/ }, allow_blank: true
+  validates :relation, format: { with: /\A[a-zA-Z\'.-]+\z/ }, allow_blank: true
 end
