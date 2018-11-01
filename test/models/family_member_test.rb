744e443d2de7e1baa7893cd862814de6ce150f4b
radabaugh <radabaughtyler24@gmail.com>
2018-10-31 19:53:21 -0700

    Added test to ensure family members respond to relation

--- test/models/family_member_test.rb
+++ test/models/family_member_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class FamilyMemberTest < ActiveSupport::TestCase
-  test "has a name" do
+  test "resonds to name" do
     family_member = family_members(:valid)
     assert_respond_to(family_member, :name)
   end
 
+  test "resonds to relation" do
+    family_member = family_members(:valid)
+    assert_respond_to(family_member, :relation)
+  end
 end
