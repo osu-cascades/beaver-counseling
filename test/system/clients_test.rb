63031e9f99d56e793c616b85973815d1df4b1fcf
Tyler Radabaugh <radabaughtyler24@gmail.com>
2019-01-15 14:07:47 -0800

    Merge Development into Master (#156)
    * Clean up Welcome page and add bootstrap.
    
    * Updated look of new client form
    
    * Allow the client to specify 0 members in the household
    
    * Update the UI of the forms filled by the client
    
    * Fixed #146 column spacing
    fixed merge conflict.
    
    * Added stuff for bootstrap.
    
    * Fixed #148 Removed form tags that were stopping the form from submitting
    
    * Fixed #149 Removed autofill from new client form
    
    * Update new clients form (#151)
    
    * Added id to client_phone_number form field
    
    * Fixed #150 Added system tests to test new clients form.
    
    * Update DB creds for heroku
    
    * Removed the Please do not fill out header at the top of the Client Information Sheet. Change the cap number of family members in a household to 10.

--- test/system/clients_test.rb
+++ test/system/clients_test.rb
@@ -1,9 +1,61 @@
 require "application_system_test_case"
 
 class ClientsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit clients_url
-  #
-  #   assert_selector "h1", text: "Client"
-  # end
+	test "new client fills out form correctly" do
+    visit 'clients/new'
+
+    fill_in 'client_phone_number', with: '555-123-1234'
+    select 'Cell', :from => 'client_phone_type'
+    check 'client_leave_message'
+    fill_in 'client_email', with: 'example@mail.com'
+    check 'client_email_leave_message'
+    fill_in 'client_emergency_name', with: 'Max'
+    fill_in 'client_emergency_phone', with: '555-321-4321'
+    fill_in 'client_emergency_relation', with: 'Father'
+    fill_in 'client_insurance_company', with: 'Farmers'
+    fill_in 'client_first_name', with: 'Stephen'
+    fill_in 'client_last_name', with: 'Smith'
+    fill_in 'client_local_address', with: '123 NW 2nd St.'
+    fill_in 'client_occupation', with: 'Barista'
+    fill_in 'client_employer', with: 'Starbucks'
+    page.execute_script("$('client_dob').val('21/12/1990')")
+    fill_in 'client_highest_edu', with: 'High school'
+    fill_in 'client_identified_gender', with: 'Male'
+    fill_in 'client_culture_background', with: 'White'
+    fill_in 'client_sexual_orientation', with: 'Straight'
+    fill_in 'client_relationship', with: 'single'
+    uncheck 'client_counselor_seen_before'
+    find('input[type="submit"]').click
+
+    assert(has_text?('Client was successfully created.'))
+  end
+
+  test "new client fills out form incorrectly" do
+    visit 'clients/new'
+
+    fill_in 'client_phone_number', with: '5551231234'
+    select 'Cell', :from => 'client_phone_type'
+    check 'client_leave_message'
+    fill_in 'client_email', with: 'example@mail.com'
+    check 'client_email_leave_message'
+    fill_in 'client_emergency_name', with: 'Max'
+    fill_in 'client_emergency_phone', with: '555-321-4321'
+    fill_in 'client_emergency_relation', with: 'Father'
+    fill_in 'client_insurance_company', with: 'Farmers'
+    fill_in 'client_first_name', with: 'Stephen'
+    fill_in 'client_last_name', with: 'Smith'
+    fill_in 'client_local_address', with: '123 NW 2nd St.'
+    fill_in 'client_occupation', with: 'Barista'
+    fill_in 'client_employer', with: 'Starbucks'
+    page.execute_script("$('client_dob').val('21/12/1990')")
+    fill_in 'client_highest_edu', with: 'High school'
+    fill_in 'client_identified_gender', with: 'Male'
+    fill_in 'client_culture_background', with: 'White'
+    fill_in 'client_sexual_orientation', with: 'Straight'
+    fill_in 'client_relationship', with: 'single'
+    uncheck 'client_counselor_seen_before'
+    find('input[type="submit"]').click
+
+    refute(has_text?('Client was successfully created.'))
+  end
 end
