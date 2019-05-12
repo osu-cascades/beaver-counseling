aa919b2466fbae9e062f2a81a11be82aae56a5fa
Krishan <krishanchopra30@yahoo.com>
2019-05-12 15:52:31 -0700

    Started working on the system test for session ratings. Still incomplete

--- test/system/session_ratings_test.rb
+++ test/system/session_ratings_test.rb
@@ -1,9 +1,19 @@
 require "application_system_test_case"
 
 class SessionRatingsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit session_ratings_url
-  #
-  #   assert_selector "h1", text: "SessionRating"
-  # end
+
+  test "client fills out session rating form correctly" do
+    visit 'session_ratings/new'
+
+    select '133', :from => 'roomNum'
+    #select '4', :from => 'rb-1'
+    fill_in 'optionalInfo', with: 'Some message given to the Counselor'
+    #find('input[type="send"]').click
+
+    assert(has_text?('Client has successfully submitted session rating.'))
+  end
+  
+  test "client fills out session rating form incorrectly" do
+    visit 'session_ratings/new'
+  end
 end
